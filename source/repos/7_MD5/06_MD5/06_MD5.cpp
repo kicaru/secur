0ccefee02e42605711f4e42bb2de1476f26e8452
kicaru <kicaru00@gmail.com>
2019-02-16 00:52:43 +0700

    complete MD5

--- source/repos/7_MD5/06_MD5/06_MD5.cpp
+++ source/repos/7_MD5/06_MD5/06_MD5.cpp
@@ -8,55 +8,25 @@
 #include <iostream>
 #include <openssl/md5.h>
 #include <string>
+#include <nlohmann/json.hpp>
+#include <iostream>
+#include <fstream>
+#include <iomanip>  
 using namespace std;
+using json = nlohmann::json;
 
 struct tokens{
 	string ID;
 	string password;
-	tokens *next;
 };
 tokens *start;
 
-void insert(string id,string pwd)
-{
-	tokens *t = start;
-	if (start != NULL)
-	{
-		while (t->next != NULL)
-		{
-			t = t->next;
-		}
-		tokens *n = new tokens;
-		t->next = n;
-		n->ID = id;
-		n->password = pwd;
-		n->next = NULL;
-	}
-	else
-	{
-		tokens *n = new tokens;
-		n->ID = id;
-		n->password = pwd;
-		n->next = NULL;
-		start = n;
-	}
-}
-
-int main()
-{
+string MD5encrypt(string txt) {
 	unsigned char digest[16];
-	string id;
-	string password;
-	cout << "id : " ;
-	getline(cin, id);
-	cout << "Password : " ;
-	getline(cin, password);
 	char string[100];
 	char mdString[67];
-	strcpy_s(string, password.c_str());
-	
+	strcpy_s(string, txt.c_str());
 
-	/* approach 2 */
 	MD5_CTX context;
 	MD5_Init(&context);
 	MD5_Update(&context, string, strlen(string));
@@ -64,11 +34,62 @@ int main()
 	for (int i = 0; i < 16; i++) {
 		sprintf_s(&mdString[i * 2], 34, "%02x", (unsigned int)digest[i]);
 	}
-	cout << "ID : " << id << endl;
-	cout << "PassWord : " << mdString << endl;
-	
+	return mdString;
+}
+
+int main()
+{
+	string id;
+	string password;
+	string log_id;
+	string log_pass;
+	int cdt=0;
+	while (cdt != 3) {
+		cout << endl;
+		cout << "log in press 1" << endl;
+		cout << "Register press 2" << endl;
+		cout << "Exit press 3" << endl;
+		cin >> cdt;
+		cout << endl;
+		if (cdt == 2) {
+			cout << "id : ";
+			cin >> id;
+			cout << "Password : ";
+			cin >> password;
+			json user;
+			password = MD5encrypt(password);
+			cout << "ID : " << id << endl;
+			cout << "PassWord : " << password << endl;
 
 
+			user["ID"] = id;
+			user["PassWord"] = password;
+
+
+			std::ofstream o("D:\\MD5\\data.json");
+			o << std::setw(4) << user << std::endl;
+
+		}
+		else if(cdt==1){
+
+
+			cout << "----------------------log in----------------" << endl;
+			cout << "ID ";
+			cin >> log_id;
+			cout << "password ";
+			cin >> log_pass;
+			log_pass = MD5encrypt(log_pass);
+			std::ifstream i("D:\\MD5\\data.json");
+			json j;
+			i >> j;
+			if (j["ID"] == log_id && j["PassWord"] == log_pass) {
+				cout << "Log in Success!!" << endl;
+			}
+			else {
+				cout << "ID or Password Incorrect" << endl;
+			}
+		}
+	}
 	return (0);
 }
 
